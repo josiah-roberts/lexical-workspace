b6d38a5b8aea0831bd8f186bd94d4f56175c9ac1
Josiah Roberts <josiah.david.roberts@gmail.com>
2022-03-09 14:22:05 -0800

    Init for lesson 1

---
+++ 1-identifiers-declaration-assignment/src/index.ts
@@ -0,0 +1,11 @@
+// loads the question function
+// import { question } from 'readline-sync';
+
+console.info("Yo! It works!");
+
+// question() is a function that reads user input and returns it as a string,
+// similar to how console.log() takes a string and writes it to the console
+
+// As the user for their name with console.info(<message>)
+// Read the user's input into a variable you declare with question()
+// Use that variable + console.info to greet them, 'Hello, <name>!'
